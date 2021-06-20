441281988ecd7862b65ebb75572375b76b97aee8
Cristian <cristian.frontend.dev@gmail.com>
2021-06-20 12:10:16 -0500

    mocks and tests created. jestdom and history installed

---
+++ src/__test__/components/Footer.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import Footer from '../../components/Footer';
+
+describe('<Footer />', () => {
+  const footer = mount(<Footer />);
+  test('Render del componente Footer', () => {
+    expect(footer.length).toEqual(1);
+  });
+
+  test('Render del título', () => {
+    expect(footer.find('.Footer-title').text()).toEqual('Platzi Store');
+  });
+});
